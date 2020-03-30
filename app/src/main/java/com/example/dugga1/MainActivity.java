9dc314795d99ccb2accb0867a7ac76f129f6ad99
Gustav Bäckman <42798203+gustav123b@users.noreply.github.com>
2020-03-30 14:48:33 +0200

    hej

---
+++ app/src/main/java/com/example/dugga1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.dugga1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "onCreate() kördes", Toast.LENGTH_LONG).show();
+    }
+}
